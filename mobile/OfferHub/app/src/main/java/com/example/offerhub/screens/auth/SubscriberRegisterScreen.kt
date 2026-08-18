a2e8cd62f1894c3f7fa4ddcf82dd617d8d4b1c04
serra888 <serranurturkoglu@gmail.com>
2026-08-19 00:53:21 +0300

    Update SubscriberRegisterScreen.kt

--- mobile/OfferHub/app/src/main/java/com/example/offerhub/screens/auth/SubscriberRegisterScreen.kt
+++ mobile/OfferHub/app/src/main/java/com/example/offerhub/screens/auth/SubscriberRegisterScreen.kt
@@ -1,2 +1,45 @@
 package com.example.offerhub.screens.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SubscriberRegisterScreen(
+    onRegisterClick: (
+        firstName: String,
+        lastName: String,
+        gsm: String,
+        email: String
+    ) -> Unit,
+    onLoginClick: () -> Unit
+){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    horizontal = 32.dp,
+                    vertical = 48.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+
+        }
+    }
+}
