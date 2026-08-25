3fa76a6a998ba2dd7f6a4934fd21c3a0657f5362
serra888 <serranurturkoglu@gmail.com>
2026-08-25 21:12:04 +0300

    Update ApiResponse.kt

--- mobile/OfferHub/app/src/main/java/com/example/offerhub/data/network/ApiResponse.kt
+++ mobile/OfferHub/app/src/main/java/com/example/offerhub/data/network/ApiResponse.kt
@@ -1,6 +1,10 @@
 package com.example.offerhub.data.network
 
-data class ApiResponse<T>(val success: Boolean, val data: T?, val error: ApiError?)
+data class ApiResponse<T>(
+    val success: Boolean,
+    val data: T?,
+    val error: ApiError?
+)
 
 data class ApiError(
     val code: String,
